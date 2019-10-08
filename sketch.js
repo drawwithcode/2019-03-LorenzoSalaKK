e303cf9dde1c2d37ad289abe6aa357f83d6a51e9
LorenzoSalaKK <salalorenzo.ls@gmail.com>
2019-10-08 23:40:31 +0200

    V1

--- sketch.js
+++ sketch.js
@@ -1,11 +1,54 @@
-function preload(){
-  // put preload code here
-}
+var myTrueBall;
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  myTrueBall = new Ball(random(0.1,windowWidth-0.1), 0, 20);
 }
 
 function draw() {
-  // put drawing code here
+  var rectW = 200 - (frameCount/50);
+
+  background('black');
+
+  push();
+  myTrueBall.move();
+  myTrueBall.display();
+  pop();
+
+  if ((myTrueBall.x < mouseX + (rectW/2) && myTrueBall.x > mouseX - (rectW/2)) && (myTrueBall.y >= windowHeight * 6 / 7 && myTrueBall.y <= windowHeight * 6.05 / 7)) {
+    myTrueBall.increaseY = -myTrueBall.increaseY;
+  };
+
+  if (myTrueBall.x < 1 || myTrueBall.x > windowWidth) {
+    myTrueBall.increaseX = -myTrueBall.increaseX;
+  } else if (myTrueBall.y < 1) {
+    myTrueBall.increaseY = -myTrueBall.increaseY;
+  };
+
+  rect(mouseX - (rectW/2), windowHeight * 6 / 7, rectW, 10);
+
+}
+
+function Ball(_x, _y, _d) {
+  //properties
+  this.size = _d;
+  this.x = _x;
+  this.y = _y;
+  //hardcoded properties
+  this.color = 'red';
+  this.increaseX = 5;
+  this.increaseY = 5;
+  //methods
+  this.move = function() {
+    this.x += this.increaseX;
+    this.y += this.increaseY;
+  }
+
+  this.display = function() {
+    fill(this.color);
+    noStroke()
+    ellipse(this.x, this.y, this.size);
+  }
+
+
 }
